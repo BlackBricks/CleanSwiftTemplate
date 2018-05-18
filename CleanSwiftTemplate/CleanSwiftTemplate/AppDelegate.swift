5e16dbaa0505093c83556c8c03530357272939e0
Kryndach <kryndach.ios@gmail.com>
2018-05-18 18:09:40 +0600

    added folders

---
+++ CleanSwiftTemplate/CleanSwiftTemplate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CleanSwiftTemplate
+//
+//  Created by Egor Kryndach on 26/03/2018.
+//  Copyright © 2018 blackbricks. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
